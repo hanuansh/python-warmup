afd5531506330d8595b2de8ec7648c6fe17696ed
Hanuansh Sharma <sharmahanuansh@gmail.com>
2025-12-13 20:26:00 +0530

    Add menu driven calculator

---
+++ pythonbeg.py
@@ -0,0 +1,16 @@
+while(True):
+    print("Calculator:\n1)Addition\n2)Subtraction\n3)Multiplication\n4)Division\n5)Exit")
+    ch=int(input("Enter your choice:"))
+    if(ch==5):
+        break
+    a=int(input("Enter operand1:"))
+    b=int(input("Enter operand2:"))
+    if(ch==1):
+        print("Addition is:",a+b)
+    elif(ch==2):
+        print("Subtraction is:",a-b)
+    elif(ch==3):
+        print("Multiplication is:",a*b)
+    elif(ch==1):
+        print("Division is:",int(a/b))
+print("Programme ended")
